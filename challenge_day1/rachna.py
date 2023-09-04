4d02cc6001d59b220f738f85070bda27dc575771
rachna-uva <126685027+rachna-uva@users.noreply.github.com>
2023-09-04 13:46:20 +0200

    Update rachna.py

--- challenge_day1/rachna.py
+++ challenge_day1/rachna.py
@@ -1,3 +1,6 @@
 def rachna():
     name = 'Rachna'
     return name
+
+def para1():
+    return("Python suddenly said, “I really wish we had a drink right now. It’s very hot and I feel like I’m about to faint!” Bob nodded in agreement. Morgan decided that they would get a hotdogs, since the stadium had recently started to sell vegan and vegetarian snacks in addition to the regular fare. When asked if she would also like a snack, Tanya shrugged her shoulders and said she wasn’t hungry, but she would like to use the restroom. Harley decided to get some nachos with Morgan, while the Gina decided to give some company to Tanya. ")
