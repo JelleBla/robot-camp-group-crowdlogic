7d6abfcdb3beee433de898595b73623f221da7c9
rachna-uva <126685027+rachna-uva@users.noreply.github.com>
2023-09-04 14:24:31 +0200

    Update rachna.py

--- challenge_day1/rachna.py
+++ challenge_day1/rachna.py
@@ -1,6 +1,15 @@
+import jelle
+import yuyue
+import luca
+import karolina
+import alexia
+
 def rachna():
     name = 'Rachna'
     return name
 
-def para1():
-    return("Python suddenly said, “I really wish we had a drink right now. It’s very hot and I feel like I’m about to faint!” Bob nodded in agreement. Morgan decided that they would get a hotdogs, since the stadium had recently started to sell vegan and vegetarian snacks in addition to the regular fare. When asked if she would also like a snack, Tanya shrugged her shoulders and said she wasn’t hungry, but she would like to use the restroom. Harley decided to get some nachos with Morgan, while the Gina decided to give some company to Tanya. ")
+def character():
+    return('Tanya')
+    
+def chapter1():
+    return("{yuyue.character()} suddenly said, “I really wish we had a drink right now. It’s very hot and I feel like I’m about to faint!” {jelle.character()} nodded in agreement. {alexia.character()} and {karolina.character()} decided that they would get a hotdogs, since the stadium had recently started to sell vegan and vegetarian snacks in addition to the regular fare. When asked if she would also like a snack, {character()} shrugged her shoulders and said she wasn’t hungry, but she would like to use the restroom. {luca.character()} decided to give some company to {character()}. ")
