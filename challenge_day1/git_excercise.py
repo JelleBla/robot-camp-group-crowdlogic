ea56c97f191b9d7a889a488056d3199df6503c5c
ErLucaBaz <lucabausani@gmail.com>
2023-09-04 15:11:14 +0200

    acapo

--- challenge_day1/git_excercise.py
+++ challenge_day1/git_excercise.py
@@ -14,7 +14,7 @@ def act1():
     print(alexia.chapter1())
     print(rachna.chapter1())
     print(karolina.chapter1())
-    print(luca.chapter1())
+    print(luca.chapter1(),"\n")
 
 def act2():
     print(jelle.chapter2())
@@ -22,7 +22,7 @@ def act2():
     print(alexia.chapter2())
     print(rachna.chapter2())
     print(karolina.chapter2())
-    print(luca.chapter2())
+    print(luca.chapter2(),"\n")
     
 def act3():
     print(jelle.chapter3())
@@ -30,7 +30,7 @@ def act3():
     print(alexia.chapter3())
     print(rachna.chapter3())
     print(karolina.chapter3())
-    print(luca.chapter3())
+    print(luca.chapter3(),"\n")
     
 teamname()
 act1()
