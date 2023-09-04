5d07950de8459f50a9d088fafdb2941de1d3c011
JelleBla <jelle-blankendaal@hotmail.com>
2023-09-04 14:26:53 +0200

    story_jelle

--- challenge_day1/jelle.py
+++ challenge_day1/jelle.py
@@ -1,6 +1,30 @@
+import yuyue
+import rachna
+import luca
+import karolina
+import alexia
+
+
 def jelle():
     name = 'Jelle'
     return name
 
+def character():
+    name = 'Bob'
+    return name
 
+def chapter1():
+    text = f'A group of six lovers: {character()}, {yuyue.character()}, {alexia.character()}, {rachna.character()}, {karolina.character()} and {luca.character()} love going to Ajax. Today they are even more excited than usual, Ajax is playing at home in the semi-final of the Champions League against Barcelona. The atmosphere around the stadium is amazing, there are fireworks and everywhere you can see there are people singing. The group would have to waitin line to enter the stadium, but that is okay. They sing along with the other fans entering thestadium, as they get pushed together walking up the stairs. They have some time before the gamestarts, so they decide to get some drinks.'
+    return text
 
+def chapter2():
+    text = f'{luca.character()}, {character()} and {rachna.character()} only have eye for each other while exiting the stadium.They loved the game, but realize they love each other more. {karolina.character()}, {alexia.character()} and {yuyue.character()}however got carried away singing together with the other Ajax Ultras. At some point{karolina.character()} realized that she could not see {luca.character()}, {character()} and {rachna.character()} anymore, but she thinks theyprobably went to the toilets together as they seemed to be looking at each other more than at the field. '
+    return text
+
+def chapter3():
+    text = f'Suddenly they start hearing voices: ‘Train to Urk leaves in 20 minutes from track 2!’.{karolina.character()} and {yuyue.character()} realize at the same time, from different places that they need to catch that train. It is however impossible to move in the right direction. {luca.character()} could not hear the voice yet, as she was still crying. {character()} was in shock of the scenes he was seeing from the other side of the fence. {alexia.character()} and {rachna.character()} are only able to think about each other, but luckily the voice is reaching them now too, bringing them back to reality. They think about Urk, and all the beautiful memories the six of themhave together there. They too start moving to the right direction. At the same time someone walksinto {luca.character()}, and asks her if she is okay and where she needs to go. She tells the young boy, he doesn’tlook older than 14, that she has to go to Urk. He says he heard a man shouting that the train to Urkleaves from track 2 at Station Bijlmer Arena. She stands up, straightens her back, thinks about the other 5 and follows the crowd to the station. {character()} too has now heard the voice and starts heading towards the station.'
+    return text
+
+print(chapter1())
+print(chapter2())
+print(chapter3())
