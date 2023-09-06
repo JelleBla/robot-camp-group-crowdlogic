60f48a079f44c0fd19c096abd1f8c737e2942dd1
karolinadao <126684668+karolinadao@users.noreply.github.com>
2023-09-06 12:07:40 +0200

    Update station4.py

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,7 +1,6 @@
 def solution_station_4(input):
-    if n > 1:
-	for i in range(2, int(n/2)+1):
-		if (n % i) == 0:
+	for i in range(2, int(input/2)+1):
+		if (input % i) == 0:
 			return True
 		break
 	else:
