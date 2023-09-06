ab7b87439c4494f6d1425d514ff103a929622886
karolinadao <126684668+karolinadao@users.noreply.github.com>
2023-09-06 12:26:26 +0200

    Update station4.py

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,7 +1,10 @@
 def solution_station_4(input):
-	for i in range(2, int(input/2)+1):
-		if (input % i) == 0:
+	if input > 1:
+		for i in range(2, int(input/2)+1):
+			if (input % i) == 0:
+				return False
+			break
+		else:
 			return True
-		break
 	else:
 		return False
