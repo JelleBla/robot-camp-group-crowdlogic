8dfe18451f8f78dddddc83da9bedce046bdbf2d2
JelleBla <jelle-blankendaal@hotmail.com>
2023-09-06 12:36:25 +0200

    'sympy'

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,7 +1,12 @@
-from sympy import symbols, sympify
 def solution_station_7(observations):
-    a, b, c, d, e = symbols('a b c d e')
-    parsed_expression = sympify(observations)
-    result = parsed_expression.subs({a: 3, b: -1, c: 4, d: 7, e: 0.5})
+    # Define the values for variables a, b, c, d, e
+    a = 3
+    b = -1
+    c = 4
+    d = 7
+    e = 0.5
+
+
+    result = eval(observations)
     return float(result)
 
