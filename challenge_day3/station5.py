67c0232dad03551ee23835c7b3f41ed52599d9d7
rachna-uva <126685027+rachna-uva@users.noreply.github.com>
2023-09-06 12:03:46 +0200

    Update station5.py

---
+++ challenge_day3/station5.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def solution_station_5(observations):
+    teams = pd.read_csv(r'challenge_day3\teams.csv')
+    teams = pd.DataFrame(teams)
+
+    teamnum = teams[teams.isin([observations]).any(axis=1)]
+    return int(teamnum['Team Number'])
+
+
+#print(solution_station_5('Luca'))
