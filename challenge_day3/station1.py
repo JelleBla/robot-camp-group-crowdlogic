acd9a08eda3c178f37eed3e02e6b58f3b88bee81
chiforalexia <126684541+chiforalexia@users.noreply.github.com>
2023-09-06 11:07:03 +0200

    Update station1.py

---
+++ challenge_day3/station1.py
@@ -0,0 +1,24 @@
+# Fibbonaci's sequence
+def solution_station_1(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a = 1
+        b = 1
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+        
+
+n = int(input("Input: "))
+
+result = solution_station_1(n)
+
+print (f"Output: {result}")
+
+
+
